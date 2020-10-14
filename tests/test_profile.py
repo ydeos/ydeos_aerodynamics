3cea2203786b9a1721424210a26e4e718370b8b8
Guillaume Florent <guillaume.florent@tutanota.com>
2020-10-14 17:26:13 +0200

    Additional tests for profile.py

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -9,7 +9,7 @@ from ydeos_aerodynamics.profiles import power_law, logarithmic
 
 
 def test_power_law():
-    r"""Power-law wind profile test"""
+    r"""Power-law wind profile test."""
     wind_speed_0m = power_law(wind_speed_known=20., height_reference=10., height=0.)
     assert wind_speed_0m == 0.
 
@@ -20,14 +20,27 @@ def test_power_law():
     assert wind_speed_20m > wind_speed_10m
 
 
+def test_power_law_wrong_input():
+    r"""Wrong parameters for the power law."""
+    with pytest.raises(ValueError):
+        power_law(wind_speed_known=-20., height_reference=10., height=0.)
+    with pytest.raises(ValueError):
+        power_law(wind_speed_known=20., height_reference=-10., height=0.)
+    with pytest.raises(ValueError):
+        power_law(wind_speed_known=20.,
+                  height_reference=10.,
+                  height=0.,
+                  alpha=0.)
+
+
 def test_power_law_negative_height():
-    r"""Power-law wind profile test"""
+    r"""Power-law wind profile test."""
     with pytest.raises(ValueError):
         _ = power_law(wind_speed_known=20., height_reference=10., height=-1.)
 
 
 def test_logarithmic():
-    r"""Logarithmic wind profile test"""
+    r"""Logarithmic wind profile test."""
     wind_speed_10m = logarithmic(wind_speed_known=20., height_reference=10., height=10.)
     assert wind_speed_10m == 20.
 
@@ -36,12 +49,25 @@ def test_logarithmic():
 
 
 def test_logarithmic_zero_height():
-    r"""logarithmic wind profile test at zero height"""
+    r"""logarithmic wind profile test at zero height."""
     with pytest.raises(ValueError):
         logarithmic(wind_speed_known=20., height_reference=10., height=0.)
 
 
 def test_logarithmic_negative_height():
-    r"""logarithmic wind profile test at negative height"""
+    r"""logarithmic wind profile test at negative height."""
     with pytest.raises(ValueError):
         logarithmic(wind_speed_known=20., height_reference=10., height=-1.)
+
+
+def test_logarithmic_wrong_input():
+    r"""Bad input for the logarithmic profile."""
+    with pytest.raises(ValueError):
+        logarithmic(wind_speed_known=-20., height_reference=10., height=1.)
+    with pytest.raises(ValueError):
+        logarithmic(wind_speed_known=20., height_reference=-10., height=1.)
+    with pytest.raises(ValueError):
+        logarithmic(wind_speed_known=20.,
+                    height_reference=10.,
+                    height=1.,
+                    roughness_length=0.)
